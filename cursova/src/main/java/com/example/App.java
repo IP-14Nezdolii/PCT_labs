712275eaeb42117156e1e3a2dad98654244ea519
IP-14Nezdolii <vladislav07007@gmail.com>
2025-03-06 19:31:39 +0200

    new commit

--- cursova/src/main/java/com/example/App.java
+++ cursova/src/main/java/com/example/App.java
@@ -10,11 +10,7 @@ public class App
     public static void main( String[] args )
     {
         Bencher.bench(0);
-        // System.out.printf("%-10s %-15s %-5s%n", "ID", "Имя", "Возраст");
-        // System.out.println("--------------------------------");
-        // System.out.printf("%-10d %-15s %-5d%n", 1, "Алексей", 25);
-        // System.out.printf("%-10d %-15s %-5d%n", 2, "Мария", 30);
-        // System.out.printf("%-10d %-15s %-5d%n", 3, "Иван", 22);
+        
 
         // var list = List.of(1,2,3,4,5,6,7,8,9,10);
 
@@ -44,4 +40,10 @@ public class App
     //         }
     //     }        
     // }
+
+    // System.out.printf("%-10s %-15s %-5s%n", "ID", "Имя", "Возраст");
+        // System.out.println("--------------------------------");
+        // System.out.printf("%-10d %-15s %-5d%n", 1, "Алексей", 25);
+        // System.out.printf("%-10d %-15s %-5d%n", 2, "Мария", 30);
+        // System.out.printf("%-10d %-15s %-5d%n", 3, "Иван", 22);
 }
