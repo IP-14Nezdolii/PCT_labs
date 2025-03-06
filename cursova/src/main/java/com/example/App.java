ad218b8820b14a850a008a4ddabc472a7d8da6b7
IP-14Nezdolii <vladislav07007@gmail.com>
2025-03-06 19:30:59 +0200

    new commit

--- cursova/src/main/java/com/example/App.java
+++ cursova/src/main/java/com/example/App.java
@@ -35,13 +35,13 @@ public class App
         // System.out.println();
     }
 
-    static void shellSort(List<Integer> array) {
-        for (int s = array.size() / 2; s > 0; s /= 2) {
-            for (int i = s; i < array.size(); ++i) {
-                for (int j = i - s; j >= 0 && array.get(j) > array.get(j + s); j -= s)  {
-                    Collections.swap(array, j, j + s);
-                }
-            }
-        }        
-    }
+    // static void shellSort(List<Integer> array) {
+    //     for (int s = array.size() / 2; s > 0; s /= 2) {
+    //         for (int i = s; i < array.size(); ++i) {
+    //             for (int j = i - s; j >= 0 && array.get(j) > array.get(j + s); j -= s)  {
+    //                 Collections.swap(array, j, j + s);
+    //             }
+    //         }
+    //     }        
+    // }
 }
